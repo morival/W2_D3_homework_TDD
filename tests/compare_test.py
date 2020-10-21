074c5b286c04fe0afbfd7dd1a362af48e02a980f
Mateusz <mateusz.harlos@gmail.com>
2020-10-21 22:25:27 +0100

    Homework TDD  W2 D3

---
+++ tests/compare_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.compare import compare
+
+class TestCompare(unittest.TestCase):
+
+    def test_compare_3_1_returns_3_is_greater_than_1(self):
+        self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_2_5_returns_2_is_less_than_5(self):
+        self.assertEqual("2 is less than 5", compare(2, 5))
+
+    def test_7_7_returns_both_numbers_are_equal(self):
+        self.assertEqual("both numbers are equal", compare(7, 7))
